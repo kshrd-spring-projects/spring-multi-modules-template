2f9c0007003f1cbb02c8186c2b23b3dbb426df7c
Khorn <srengkhorn@gmail.com>
2019-03-21 18:48:03 +0700

    updated

--- web-client/src/main/java/com/khorn/App.java
+++ web-client/src/main/java/com/khorn/App.java
@@ -2,11 +2,14 @@ package com.khorn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * We have to run project from controller in web-client module
  * not parent-project
- *
+ * if SpringBoot cannot scan package of Controller, Service and Repository
+ * we use Annotation @ComponentScan to find them
+ * @ComponentScan(basePackages= {"com.khorn"})
  */
 
 @SpringBootApplication
